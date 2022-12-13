eb24ae1565e5224f6f470fcd3358a8258f70181f
Vivek Shukla <vivekshukla60572@gmail.com>
2022-12-13 18:34:17 +0530

    added useEffect and useReducer function

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,18 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useReducer/* useState */} from 'react'
 import './style.css'
+
+const reducer = (state,action) => {
+     
+     if(action.type === "INC"){
+        state = state + 1;
+     }
+     
+     if(state > 1 && action.type === "DEC"){
+      state = state - 1;
+     }
+    
+     if(action.type === "DOUB"){
+        state = 2 * state;
+      }
+    
+      if(action.type === "RESET"){
+        state = 1;
+      }
+   
+      return state;
+}
 
 const UseState = () => {
 
-    const [counter, setCounter] = useState(1);
+  // const [counter, setCounter] = useState(1);
+  
+  // useEffect feature
+
+  //  using useReducer as it works almost same as useState
+  const initialData = 1;
+  const [state, dispatch] = useReducer(reducer,initialData);
+
+  useEffect(() => {
+    document.title = `Count(${state})`;
+    // console.log("Hi");
+});
 
 
   return (
     <>
       <div className="center_div">
-        <p>{counter}</p>
-        <div onClick={() => {
+        <p>{state}</p>
+        {/* <div onClick={() => {
             setCounter(counter + 1)
-        }} 
+        }}  
+        className="button2">
+        */}
+        <div onClick={() => dispatch({type:"INC"})}
         className="button2">
             <span></span>
             <span></span>
@@ -21,10 +56,12 @@ const UseState = () => {
            INC
         </div>
 
-        <div onClick={() => {
+        {/* <div onClick={() => {
             counter > 1 ? setCounter(counter - 1) : setCounter(1)
         }}
-         className="button2">
+         className="button2"> */}
+                <div onClick={() => dispatch({type:"DEC"})}
+        className="button2">
             <span></span>
             <span></span>
             <span></span>
@@ -32,10 +69,12 @@ const UseState = () => {
            DEC
         </div>
 
-        <div onClick={() => {
+        {/* <div onClick={() => {
             setCounter(counter * 2)
         }}
-         className="button2">
+         className="button2"> */}
+         <div onClick={() => dispatch({type:"DOUB"})}
+        className="button2">
             <span></span>
             <span></span>
             <span></span>
@@ -43,9 +82,7 @@ const UseState = () => {
            DOUB
         </div>
 
-        <div onClick={() => {
-            setCounter(1)
-        }}
+        <div onClick={() => dispatch({type:"RESET"})}
          className="button2">
             <span></span>
             <span></span>
